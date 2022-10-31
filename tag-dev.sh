ff684e8ce426de15033c3f65bccf5fadc22a724a
Ratchaneekorn S <r.suwanwong@gmail.com>
2022-10-31 11:47:19 +0700

    Separate shell tag dev and qa

--- tag-dev.sh
+++ tag-dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 tag_version="v1"
 tag_sprint="1"
-branch_protected="qa"
+patch_prefix="-alpha"
+branch_protected="develop"
 
 makeSureInput() {
     echo "Are you sure ? [y/n]"
@@ -18,7 +19,7 @@ currentVersion() {
     echo "Fetch tags"
     git fetch --prune --tags
 
-    version=$(git tag -l "${tag_version}.${tag_sprint}.[0-9]*" --sort=-v:refname | head -n 1)
+    version=$(git tag -l "${tag_version}.${tag_sprint}.[0-9]${patch_prefix}.[0-9]*" --sort=-v:refname | head -n 1)
     
     if [ -z $version ]; then
         version='this first tag'
@@ -54,24 +55,31 @@ checkIsCommitsBeyondRemoteOrigin() {
 }
 
 if [ $# -eq 0 ]; then
-    echo "$(tput setaf 1)$(tput bold)Please fill arguments [major/minor]$(tput sgr 0)"
+    echo "$(tput setaf 1)$(tput bold)Please fill arguments [major/minor/patch]$(tput sgr 0)"
     exit 0
 elif [ $1 == "major" ]; then
-    echo "$(tput setaf 6)$(tput bold)Update tag version $(tput setaf 4)$(tput bold)x.x.x$(tput sgr 0)"
+    echo "$(tput setaf 6)$(tput bold)Update tag version $(tput setaf 4)$(tput bold)x.x.x-alpha.x$(tput sgr 0)"
     currentVersion
 
-    increment=(${increment/$tag_version/$tag_sprint/})
+    increment=(${increment/$tag_version/$tag_sprint/$patch_prefix/})
     increment=(${increment//./ })
 
     increment[0]=${tag_version}
     increment[1]=${tag_sprint}
-    increment[2]=0
+    increment[2]=0${patch_prefix}
+    increment[3]=0
 elif [ $1 == "minor" ]; then
-    echo "$(tput setaf 6)$(tput bold)Update tag version $(tput setaf 4)$(tput bold)x.x.minor$(tput sgr 0)"
+    echo "$(tput setaf 6)$(tput bold)Update tag version $(tput setaf 4)$(tput bold)x.x.minor-alpha.x$(tput sgr 0)"
     currentVersion
     ((increment[2]++))
+    increment[2]=${increment[2]}${patch_prefix}
+    increment[3]=0
+elif [ $1 == "patch" ]; then
+    echo "$(tput setaf 6)$(tput bold)Update tag version $(tput setaf 4)$(tput bold)x.x.x-alpha.patch$(tput sgr 0)"
+    currentVersion
+    ((increment[3]++))
 else
-    echo "$(tput setaf 1)$(tput bold)Please fill arguments [major/minor]$(tput sgr 0)"
+    echo "$(tput setaf 1)$(tput bold)Please fill arguments [major/minor/patch]$(tput sgr 0)"
     exit 0
 fi
 
@@ -81,7 +89,7 @@ git pull origin "$branch_protected"
 checkIsConflict
 checkIsCommitsBeyondRemoteOrigin
 git pull origin $(git branch | grep \* | cut -d ' ' -f2)
-next_version="${increment[0]}.${increment[1]}.${increment[2]}"
+next_version="${increment[0]}.${increment[1]}.${increment[2]}.${increment[3]}"
 msg="Update version $1 by $(git config user.name)"
 echo "$(tput setaf 6)$(tput bold)Add new git tag $(tput setaf 4)$(tput bold)$next_version $(tput setaf 6)$(tput bold)with message: $(tput setaf 4)$(tput bold)$msg $(tput sgr 0)"
 makeSureInput
